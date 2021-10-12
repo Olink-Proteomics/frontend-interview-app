e5ab38c41b1dffc03bb7b480091642005b85d512
Paul Sjöberg <paul.sjoberg@olink.com>
2021-10-12 14:09:46 +0200

    Add hook for async fetching data

---
+++ protein-explorer/src/hooks/fetch-data.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import { fetchData } from '../lib/api'
+
+export const useFetchData = (promise) => {
+    const [data, setData] = useState()
+    const [error, setError] = useState()
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        if (!promise) return
+
+        promise.then(setData)
+            .catch(setError)
+            .finally(() => setLoading(false))
+    }, [promise])
+
+    return { loading, data, error }
+}
